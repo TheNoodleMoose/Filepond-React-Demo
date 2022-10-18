6056e2d5092d0c1ac4c71c96d98c04659e204f82
Christian Huffman <cjh605@gmail.com>
2022-10-17 21:30:15 -0400

    Add filepond component to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { FilePond } from "react-filepond";
+import "filepond/dist/filepond.min.css";
+import "./App.css";
 
 function App() {
+  const [files, setFiles] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <FilePond
+        files={files}
+        onupdatefiles={(f: any) => setFiles(f)}
+        allowMultiple={true}
+        maxFiles={3}
+        server='/api'
+        name='files'
+        labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+      />
     </div>
   );
 }
